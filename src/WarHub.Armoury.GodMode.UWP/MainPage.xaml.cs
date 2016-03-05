6948c7b665f9ac21f345057ca67b9c4792e24a3d
Amadeusz Sadowski <amis.tau.92@gmail.com>
2016-03-05 13:55:01 +0100

    fixing, renaming and relocating all files

---
+++ src/WarHub.Armoury.GodMode.UWP/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿// WarHub licenses this file to you under the MIT license.
+// See LICENSE file in the project root for more information.
+
+namespace WarHub.Armoury.GodMode.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            LoadApplication(new GodMode.App());
+        }
+    }
+}
---
+++ src/WarHub.Armoury.GodMode/App.cs
@@ -0,0 +1,45 @@
+﻿// WarHub licenses this file to you under the MIT license.
+// See LICENSE file in the project root for more information.
+
+namespace WarHub.Armoury.GodMode
+{
+    using Xamarin.Forms;
+
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            MainPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children =
+                    {
+                        new Label
+                        {
+                            XAlign = TextAlignment.Center,
+                            Text = "Welcome to Xamarin Forms!"
+                        }
+                    }
+                }
+            };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
