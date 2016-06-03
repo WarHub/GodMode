bf60516e8ea55b38704fbc2541ea4a6916c104a8
Amadeusz Sadowski <amis.tau.92@gmail.com>
2016-06-03 19:19:59 +0200

    drafting data index handling

---
+++ src/WarHub.Armoury.GodMode/Modules/DataAccess/ViewModels/DataIndexViewModel.cs
@@ -0,0 +1,23 @@
+﻿// WarHub licenses this file to you under the MIT license.
+// See LICENSE file in the project root for more information.
+
+namespace WarHub.Armoury.GodMode.Modules.DataAccess.ViewModels
+{
+    using Model;
+    using Model.Repo;
+    using Mvvm;
+
+    public class DataIndexViewModel : ViewModelBase
+    {
+        public DataIndexViewModel(IDataIndexService dataIndexService)
+        {
+            DataIndexService = dataIndexService;
+        }
+
+        // TODO add open system index command
+
+        public IObservableReadonlySet<ISystemIndex> SystemIndexes => DataIndexService.SystemIndexes;
+
+        private IDataIndexService DataIndexService { get; }
+    }
+}
