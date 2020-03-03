779355aba5b033b2c4e3e587ca306eccdf7d8e03
Amadeusz Sadowski <amis.tau.92@gmail.com>
2020-03-04 00:12:56 +0100

    first working tree-view of datafile content

---
+++ src/WarHub.GodMode/Data/DatafilesService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using WarHub.ArmouryModel.ProjectModel;
+using WarHub.ArmouryModel.Source;
+using WarHub.ArmouryModel.Workspaces.BattleScribe;
+
+namespace WarHub.GodMode.Data
+{
+    public class DatafilesService
+    {
+        private readonly string dataDir;
+
+        public DatafilesService(IConfiguration configuration)
+        {
+            dataDir = configuration["DataDir"];
+        }
+
+        public IEnumerable<string> GetDatafiles()
+        {
+            return Directory.EnumerateFiles(dataDir, "*", SearchOption.AllDirectories)
+                .Where(x => x.EndsWith(".cat", StringComparison.OrdinalIgnoreCase) || x.EndsWith(".gst", StringComparison.OrdinalIgnoreCase));
+        }
+
+        public IDatafileInfo<SourceNode> GetDatafileInfo(string filename)
+        {
+            var filepath = Path.Combine(dataDir, filename);
+            return filepath.LoadSourceFileAuto();
+        }
+    }
+}
--- src/WarHub.GodMode/Startup.cs
+++ src/WarHub.GodMode/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -29,6 +29,7 @@ namespace WarHub.GodMode
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<DatafilesService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
