913a2818e78bb7d5041c80bbb83b69d46ce23f04
Amadeusz Sadowski <amis.tau.92@gmail.com>
2020-03-14 13:26:32 +0100

    style: Add stacked icon "modifiers"

--- src/WarHub.GodMode/Data/NodeDisplayService.cs
+++ src/WarHub.GodMode/Data/NodeDisplayService.cs
@@ -6,42 +6,53 @@ using WarHub.ArmouryModel.Source;
 
 namespace WarHub.GodMode.Data
 {
+    public struct NodeDisplayInfo
+    {
+        public string Icon;
+        public string Name;
+        public string IconModifier;
+    }
+
     public class NodeDisplayService
     {
-        public struct NodeDisplayInfo
-        {
-            public string Icon;
-            public string Name;
-        }
-
         public static NodeDisplayInfo GetNodeDisplayInfo(SourceNode node)
         {
             var name = node is INameableNode named ? named.Name : node.Kind.ToString();
-            var icon = node.Kind switch
+            var (icon, modifier) = node.Kind switch
             {
                 // open iconic
-                SourceKind.Catalogue => "grid-three-up",
-                SourceKind.CategoryEntry => "tag",
-                SourceKind.Condition => "cog",
-                SourceKind.Constraint => "crop",
-                SourceKind.Cost => "dollar",
-                SourceKind.ForceEntry => "project",
-                SourceKind.Gamesystem => "target",
-                SourceKind.InfoGroup => "list",
-                SourceKind.Modifier => "wrench",
-                SourceKind.Profile => "spreadsheet",
-                SourceKind.Publication => "book",
-                SourceKind.Repeat => "loop",
-                SourceKind.Roster => "document",
-                SourceKind.Rule => "text",
-                SourceKind.SelectionEntry => "file",
-                SourceKind.SelectionEntryGroup => "folder",
-                _ => "question-mark"
+                SourceKind.Catalogue => ("grid-three-up", null),
+                SourceKind.CatalogueLink => ("grid-three-up", "link-intact"),
+                SourceKind.CategoryEntry => ("tag", null),
+                SourceKind.CategoryLink => ("tag", "link-intact"),
+                SourceKind.Condition => ("cog", null),
+                SourceKind.ConditionGroup => ("cog", "list"),
+                SourceKind.Constraint => ("crop", null),
+                SourceKind.Cost => ("dollar", null),
+                SourceKind.CostLimit => ("dollar", "lock-locked"),
+                SourceKind.CostType => ("dollar", "beaker"),
+                SourceKind.EntryLink => ("link-intact", null),
+                SourceKind.ForceEntry => ("project", null),
+                SourceKind.Gamesystem => ("target", null),
+                SourceKind.InfoGroup => ("info", "list"),
+                SourceKind.InfoLink => ("info", "link-intact"),
+                SourceKind.Modifier => ("wrench", null),
+                SourceKind.ModifierGroup => ("wrench", "list"),
+                SourceKind.Profile => ("spreadsheet", null),
+                SourceKind.ProfileType => ("spreadsheet", "beaker"),
+                SourceKind.Publication => ("book", null),
+                SourceKind.Repeat => ("loop", null),
+                SourceKind.Roster => ("document", null),
+                SourceKind.Rule => ("text", null),
+                SourceKind.SelectionEntry => ("file", null),
+                SourceKind.SelectionEntryGroup => ("folder", null),
+                _ => ("question-mark", null),
             };
             return new NodeDisplayInfo
             {
                 Icon = icon,
-                Name = name
+                Name = name,
+                IconModifier = modifier
             };
         }
     }
