cd090ca843f206b5b418d0071d623280195d6e8f
Amadeusz Sadowski <amis.tau.92@gmail.com>
2020-03-14 18:14:11 +0100

    refactor: Use XmlWorkspace to load

--- src/WarHub.GodMode/Data/DatafilesService.cs
+++ src/WarHub.GodMode/Data/DatafilesService.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.Collections.Immutable;
 using Microsoft.Extensions.Configuration;
 using WarHub.ArmouryModel.ProjectModel;
-using WarHub.ArmouryModel.Source;
 using WarHub.ArmouryModel.Workspaces.BattleScribe;
 
 namespace WarHub.GodMode.Data
@@ -12,22 +8,14 @@ namespace WarHub.GodMode.Data
     public class DatafilesService
     {
         private readonly string dataDir;
+        private readonly IWorkspace workspace;
+
+        public ImmutableArray<IDatafileInfo> Datafiles => workspace.Datafiles;
 
         public DatafilesService(IConfiguration configuration)
         {
             dataDir = configuration["DataDir"];
-        }
-
-        public IEnumerable<string> GetDatafiles()
-        {
-            return Directory.EnumerateFiles(dataDir, "*", SearchOption.AllDirectories)
-                .Where(x => x.EndsWith(".cat", StringComparison.OrdinalIgnoreCase) || x.EndsWith(".gst", StringComparison.OrdinalIgnoreCase));
-        }
-
-        public IDatafileInfo<SourceNode> GetDatafileInfo(string filename)
-        {
-            var filepath = Path.Combine(dataDir, filename);
-            return filepath.LoadSourceFileAuto();
+            workspace = XmlWorkspace.CreateFromDirectory(dataDir);
         }
     }
 }
