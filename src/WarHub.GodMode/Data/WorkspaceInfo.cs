8b4774f45228ef1a2aa25b2064c561e145d1812d
Amadeusz Sadowski <amis.tau.92@gmail.com>
2020-04-15 17:09:42 +0200

    refactor: GitHubRepository calculated property

--- src/WarHub.GodMode/Data/WorkspaceInfo.cs
+++ src/WarHub.GodMode/Data/WorkspaceInfo.cs
@@ -12,9 +12,13 @@ namespace WarHub.GodMode.Data
     [Record(Features.Default | Features.Equality)]
     public sealed partial class WorkspaceInfo
     {
+        private string repository;
+
         public WorkspaceType Type { get; }
 
         public Uri GitHubUrl { get; }
+
+        public string GitHubRepository => repository ??= string.Concat(GitHubUrl?.Segments[^2..]);
 
         public static WorkspaceInfo LocalFs { get; } = new WorkspaceInfo(WorkspaceType.LocalFilesystem, null);
         public static WorkspaceInfo CreateGitHub(Uri gitHubUrl) => new WorkspaceInfo(WorkspaceType.GitHub, gitHubUrl);
